ad184ab0841069b41d4aef478bda741c62d69bf0
prshnt_algo <prshntchourasia@gmail.com>
2017-11-17 00:17:31 +0530

    solved the bug related to capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,11 +2,22 @@ set :stage, :production
 set :branch, 'master'
 
 
+# set :pty, true
+# set :ssh_options, {
+#   # forward_agent: true,
+#   # auth_methods: %w(password)
+#   # keys: ["/home/algofocus/.ssh/id_ras.pub"]
+#  #  	default_run_options[:pty] = true
+# 	# ssh_options[:forward_agent] = true
+# 	# ssh_options[:auth_methods] = ["publickey"]
+# 	# ssh_options[:keys] = ["/home/algofocus/pem_files/RegApp.pem"]
+# }
 set :pty, true
+
 set :ssh_options, {
-  # forward_agent: true,
-  # auth_methods: %w(password)
-  keys: ["/home/algofocus/.ssh/id_ras.pub"]
+  forward_agent: true,
+  auth_methods: ["publickey"],
+  keys: ["/home/algofocus/pem_files/RegApp.pem"]
 }
 server '18.216.97.200', user: 'ubuntu', roles: %w{web app db}
 # ubuntu@18.216.250.170
