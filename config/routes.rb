3b6d023ee61e775682653bb545b30075da783757
prshnt_algo <prshntchourasia@gmail.com>
2017-11-19 05:20:31 +0530

    added basic designing in the form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get 'api/get_detail'
   get 'api/get_auth_detail'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  # get "/sign_up" => "devise/registrations#new"
+
+  devise_for :users, :controllers => { registrations: 'registrations' } do
+  	get "/signin", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
+   	# get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
+   	get "/sign_up", :to => "devise/registrations#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
